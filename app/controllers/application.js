fe131c1540ce5ed10cc4c5ebba5e40ba9843a210
Arnaud SJongers <asjongers@gmail.com>
2017-11-15 13:58:57 +0100

    now fetch & select default user

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,10 +4,25 @@ export default Ember.Controller.extend({
   ajax: Ember.inject.service(),
 
   options: [
-    {name: "http://mu.semte.ch/users/principle"},
-    {name: "http://mu.semte.ch/users/teacher1"},
-    {name: "http://mu.semte.ch/users/student1"}
+    {user: "http://mu.semte.ch/users/principle"},
+    {user: "http://mu.semte.ch/users/teacher1"},
+    {user: "http://mu.semte.ch/users/student1"}
   ],
+
+  init() {
+    const _this = this;
+    let prom = Ember.$.get("/as/auth");
+    prom.catch(error => {
+      console.log("Couldn't get user");
+      _this.set('selectedOption', null);
+    });
+    prom.then(result => {
+      if(result) {
+        _this.set('selectedOption', result);
+      }
+    });
+    this._super();
+  },
 
   selectedOption: null,
 
@@ -15,9 +30,9 @@ export default Ember.Controller.extend({
     setOption(option) {
       const _this = this;
       this.set('selectedOption', option);
-      let prom = Ember.$.post("/as/auth", { user: option.name });
+      let prom = Ember.$.post("/as/auth", { user: option.user });
       prom.catch(error => {
-        console.log("error");
+        console.log("Couldn't post new user.");
         _this.send('userChanged');
       });
       return prom.then(result => {
