12da2b967bc257ac09bbd19fc596422b4e4a5a8b
Arnaud SJongers <asjongers@gmail.com>
2017-11-15 16:33:11 +0100

    refresh all relations + error page

--- app/mixins/user-changed.js
+++ app/mixins/user-changed.js
@@ -3,7 +3,29 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   actions: {
     userChanged(){
-      this.refresh();
+      let model = this.modelFor(this.get('router').currentRouteName);
+      if(!model){ this.refresh(); return null;}
+
+      let refresh = function(record){
+        return record.reload().then(record => {
+          if(!record) {return null;}
+          return record.eachRelationship(function(name, descriptor) {
+            record[descriptor.kind](name).reload();
+          });
+        });
+      };
+      if(!model.forEach)
+      {
+        refresh(model);
+        return this.refresh();
+      }
+      else
+      {
+        model.forEach(item => {
+          refresh(item);
+        });
+        return this.refresh();
+      }
     }
   }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  actions: {
+    error(error, transition) {
+      let controller = this.controllerFor('error');
+      controller.set('id', transition.params[transition.targetName].id);
+      controller.set('error', error);
+      return true;
+    }
+  }
 });
